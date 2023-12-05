da2cd8e5e032f60bef8e57ec1029da9a3aabfb60
thehuglio <122838549+thehuglio@users.noreply.github.com>
2023-12-05 22:15:32 +0100

    Update Day05.java

--- Day05.java
+++ Day05.java
@@ -58,3 +58,116 @@ public class Main {
         return convert;
     }
 }
+//part 2
+package org.thehuglio;
+
+import java.io.File;
+import java.util.*;
+
+public class Main {
+
+    private static final List<String> data = new Reader(new File("data.txt")).data;
+    private static long lowest = 2000000000;
+
+    public static void main(String[] args) {
+        List<List<List<Long>>> datalists = new LinkedList<>();
+        List<List<Long>> ranges = new LinkedList<>();
+        List<List<Long>> templist = new LinkedList<>();
+
+        String[] seedsplit = data.get(0).split(": ")[1].split(" ");
+
+        Long lowest = Long.parseLong("10000000000000");
+
+        for (int i = 3; i < data.size(); i++) {
+            String datastring = data.get(i);
+            if (Objects.equals(datastring, "")) {
+                i++;
+                datalists.add(templist);
+                templist = new LinkedList<>();
+                continue;
+            }
+            List<Long> temp = new LinkedList<>();
+            for (String s : datastring.split(" ")) {
+                temp.add(Long.parseLong(s));
+            }
+            templist.add(temp);
+        }
+        datalists.add(templist);
+
+        for (int i = 0; i < seedsplit.length; i += 2) {
+            List<Long> temp = new LinkedList<>();
+            temp.add(Long.parseLong(seedsplit[i]));
+            temp.add(Long.parseLong(seedsplit[i]) + Long.parseLong(seedsplit[i + 1]));
+            ranges.add(temp);
+        }
+        for (List<List<Long>> datalist : datalists) {
+            List<List<Long>> tempranges = new LinkedList<>();
+            for (List<Long> range : ranges) {
+                List<Long> temprange = new LinkedList<>();
+                int i = 0;
+                while (i < range.size()) {
+                    boolean check = true;
+                    long low = range.get(i);
+                    long high = range.get(i + 1);
+                    for (List<Long> data : datalist) {
+                        long destination = data.get(0);
+                        long sorce = data.get(1);
+                        long marge = data.get(2);
+                        if (sorce + marge < low);
+                        else if (sorce > high);
+                        else if (sorce <= low && sorce + marge >= high) {
+                            temprange.add(destination + low - sorce);
+                            temprange.add(destination + high - sorce);
+                            check = false;
+                            break;
+                        }
+                        else if (sorce <= low && sorce + marge < high) {
+                            temprange.add(destination + (low - sorce));
+                            temprange.add(destination + marge);
+                            range.add(sorce + marge + 1);
+                            range.add(high);
+                            check = false;
+                            break;
+                        } else if (sorce > low && sorce + marge >= high) {
+                            temprange.add(destination);
+                            temprange.add(destination + high - sorce);
+                            range.add(low);
+                            range.add(sorce - 1);
+                            check = false;
+                            break;
+                        }
+                        else if (sorce > low && sorce + marge < high) {
+                            range.add(low);
+                            range.add(sorce - 1);
+                            temprange.add(destination);
+                            temprange.add(destination + marge);
+                            range.add(sorce + marge + 1);
+                            range.add(high);
+                            check = false;
+                            break;
+                        } else {
+                            System.out.println("error 404");
+                        }
+                    }
+                    if (check) {
+                        temprange.add(low);
+                        temprange.add(high);
+                    }
+                    i += 2;
+                }
+                tempranges.add(temprange);
+            }
+            ranges = tempranges;
+            System.out.println(tempranges);
+        }
+        for (List<Long> tempranges : ranges) {
+            for (Long temprange : tempranges) {
+                if (temprange < lowest) {
+                    lowest = temprange;
+                }
+            }
+        }
+        System.out.println(lowest - 1); //i dont know why this -1 is here but it worked so im not going to touch it
+    }
+}
+
