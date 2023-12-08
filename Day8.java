17d3516fd67465b65c9e48836295cd0f3365cd5a
thehuglio <122838549+thehuglio@users.noreply.github.com>
2023-12-08 22:42:11 +0100

    Update Day8.java

--- Day8.java
+++ Day8.java
@@ -61,7 +61,7 @@ public class Main {
     }
 }
 
-//part 2
+//part 2 (not finished
 package org.thehuglio;
 
 import java.io.File;
@@ -75,7 +75,6 @@ public class Main {
     public static void main(String[] args) {
         Directions directions = new Directions();
         List<String> current = new LinkedList<>();
-        List<String> end = new LinkedList<>();
         for (int i = 2; i < data.size(); i++) {
             if (!data.get(i).equals("")) {
                 String[] split = data.get(i).split(" = ");
@@ -83,31 +82,77 @@ public class Main {
                 if (split[0].endsWith("A")) {
                     current.add(split[0]);
                 }
-                if (split[0].endsWith("Z")) {
-                    end.add(split[0]);
-                }
             }
         }
         String[] rl = data.get(0).split("");
-        long tot = 0;
+        int tot = 0;
         boolean check = true;
+        List<List<Integer>> loops = new LinkedList<>();
+        List<Integer> done = new LinkedList<>();
         while (check) {
-            List<String> temp = new LinkedList<>();
-            for (String s : current) {
-                for (String direction : rl) {
+            for (String direction : rl) {
+                List<String> temp = new LinkedList<>();
+                for (String s : current) {
                     temp.add(directions.getDirections(s, direction));
                 }
-            }
-            tot++;
-            for (String s : temp) {
-                if (!end.contains(s)) {
-                    check = true;
-                    break;
+                tot++;
+                for (int i = 0; i < temp.size(); i++) {
+                    if (!done.contains(i)) {
+                        if (temp.get(i).endsWith("Z")) {
+                            int tempi = 0;
+                            if (loops.size() <= i) {
+                                loops.add(List.of(tot));
+                            } else {
+                                for (int j : loops.get(i)) {
+                                    tempi += j;
+                                }
+                                int t = tot - tempi;
+                                if (loops.get(i).contains(t)) {
+                                    done.add(i);
+                                } else {
+                                    List<Integer> templist = new LinkedList<>(loops.get(i));
+                                    templist.add(t);
+                                    loops.set(i, templist);
+                                }
+                            }
+                        }
+                        check = true;
+                    }
+                    if (done.size() == current.size()) {
+                        List<Long> lastlist = new LinkedList<>();
+                        for (List<Integer> loop : loops) {
+                            lastlist.add(Long.valueOf(loop.get(0)));
+                        }
+                        List<Integer> counter = new LinkedList<>(List.of(0,0,0,0,0,0,0));
+                        boolean check1 = true;
+                        long firstint = 0;
+                        while (check1) {
+                            for (int j = 0; j < loops.size(); j++) {
+                                List<Integer> loop = loops.get(j);
+                                for (int k = 0; k < loops.size(); k++) {
+                                    if (lastlist.get(j) < lastlist.get(k)) {
+                                        lastlist.set(j, lastlist.get(j) + loop.get(counter.get(j) % (loop.size() - 1) + 1));
+                                        counter.set(j, counter.get(j) + 1);
+                                    } else {break;}
+                                }
+                            }
+                            firstint = lastlist.get(0);
+                            for (long j : lastlist) {
+                                if (j != firstint) {
+                                    check1 = true;
+                                    break;
+                                }
+                                check1 = false;
+                            }
+                        }
+                        System.out.println(firstint);
+                        check = false;
+                        break;
+                    }
                 }
-                check = false;
+                current = temp;
+                if (!check) { break; }
             }
-            current = temp;
         }
-        System.out.println(tot);
     }
 }
